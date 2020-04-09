5666fd51b5ea508c2db27bd980190a7daade35d2
Philipp Gottschalk <gottschalk.philipp@web.de>
2020-04-10 01:00:09 +0200

    update

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,24 @@
 import React from "react"
+import { Link } from "gatsby"
+const ListLink = props => (
+  <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+    <Link to={props.to}>{props.children}</Link>
+  </li>
+)
 
 export default ({ children }) => (
   <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
+    <header style={{ marginBottom: `1.5rem` }}>
+      <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
+        <h3 style={{ display: `inline` }}>Pippo.tech</h3>
+      </Link>
+      <ul style={{ listStyle: `none`, float: `right` }}>
+        <ListLink to="/">Home</ListLink>
+        <ListLink to="/about/">About</ListLink>
+        <ListLink to="/contact/">Contact</ListLink>
+        <a href="https://www.blog.pippo.tech/">Blog</a>
+      </ul>
+    </header>
     {children}
   </div>
 )
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,10 @@
 import React from "react"
 import Layout from "../components/layout"
+import error from "../content/assets/404.png"
 
 export default () => (
 <Layout>
-    <h1>About me</h1>
-
+    <h1>404 - Page not found.</h1>
+    <img src={error}></img>
 </Layout>
 )
