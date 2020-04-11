a11283bf365dec7bbdd28afed5a1128e8abb4144
PhilGsk <gottschalk.philipp@web.de>
2020-04-11 10:49:57 +0200

    updated email

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,7 +5,7 @@ export default () => (
 <Layout>
     <h1>I'd love to talk! Email me at the address below</h1>
     <p>
-      <a href="mailto:me@example.com">me@example.com</a>
+      <a href="mailto:me@example.com">mail@philgsk.de</a>
     </p>
 </Layout>
 )
