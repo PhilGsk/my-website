ef83322168410c58c683d228fb708c15b5688923
Philipp Gottschalk <philipp.gottschalk@adesso.de>
2022-01-26 17:08:19 +0100

    work

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
+import Scene from "../components/scene"
 
 export default () => (
-<Layout>
-    <h1>About me</h1>
-
-</Layout>
+<Scene/>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,18 @@ import Scene from "../components/scene"
 import Layout from "../components/layout"
 
 export default () => (
-<Layout>
-<Scene />
-</Layout>
+<div className="curtain">
+  <div className="curtain__wrapper">
+    <input type="checkbox" defaultChecked />
+    <div className="curtain__panel curtain__panel--left">
+      <h1>Click&nbsp;</h1>
+    </div> {/* curtain__panel */}
+    <div className="curtain__content">
+      <img src="https://cdn-icons.flaticon.com/png/512/3587/premium/3587361.png?token=exp=1643208348~hmac=296dea0324cd4bd176ecd2727fb29b46" />
+    </div>
+    <div className="curtain__panel curtain__panel--right">
+      <h1>me...</h1>
+    </div> {/* curtain__panel */}
+  </div> {/* curtain__wrapper */}
+</div>
 )
